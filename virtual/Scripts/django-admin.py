d8e90b2005343c7c60f2eb4681f3e959ddd172d9
Maksim <mitsuk.maksim@gmail.com>
2020-05-02 01:27:54 +0300

    My Django app, first commit

---
+++ virtual/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mitsu\PycharmProjects\django_girls\virtual\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
